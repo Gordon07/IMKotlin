a560c7a7b2996c03c1928f1a6363d14de608fcf0
gordon <gordon_sun07@163.com>
2020-03-22 17:36:28 +0800

    创建项目协议包及BasePresenter

---
+++ app/src/main/java/com/gordon/kotlin_im/contract/BasePresenter.kt
@@ -0,0 +1,12 @@
+package com.gordon.kotlin_im.contract
+
+/**
+ *    author : gordon
+ *    e-mail : gordon_sun07@163.com
+ *    date   : 2020/3/22 17:35
+ *    version: 1.0
+ *    desc   :
+ *
+ */
+interface BasePresenter {
+}
