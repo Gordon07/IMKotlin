ef61b8d6bb8408b6effce50a6889ee4cf1a9c1b3
gordon <gordon_sun07@163.com>
2020-03-22 17:54:46 +0800

    1.闪屏页MVP-View层实现 2.anko依赖

---
+++ app/src/main/java/com/gordon/kotlin_im/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.gordon.kotlin_im
+
+/**
+ *    author : gordon
+ *    e-mail : gordon_sun07@163.com
+ *    date   : 2020/3/22 17:45
+ *    version: 1.0
+ *    desc   : 创建登录界面
+ *
+ */
+class LoginActivity:BaseActivity() {
+
+    override fun getLayoutResId(): Int {
+        return R.layout.activity_login
+    }
+}
--- app/src/main/java/com/gordon/kotlin_im/SplashActivity.kt
+++ app/src/main/java/com/gordon/kotlin_im/SplashActivity.kt
@@ -1,5 +1,9 @@
 package com.gordon.kotlin_im
 
+import android.os.Handler
+import com.gordon.kotlin_im.contract.SplashContract
+import org.jetbrains.anko.startActivity
+
 /**
  *    author : gordon
  *    e-mail : gordon_sun07@163.com
@@ -8,8 +12,44 @@ package com.gordon.kotlin_im
  *    desc   :
  *
  */
-class SplashActivity : BaseActivity() {
+class SplashActivity : BaseActivity(),SplashContract.View {
+
+    //定义常量
+    companion object {
+        val DELAY = 2000L
+    }
+
+    //定义handler
+    val handler by lazy {
+        Handler()
+    }
+
     override fun getLayoutResId(): Int {
         return R.layout.activity_splash
+    }
+
+    override fun onNoLoggedIn() {
+        //跳转登录界面
+        //方法1：匿名对象用object
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                //依赖anko库实现跳转函数
+                startActivity<LoginActivity>()
+                finish()
+            }
+        }, DELAY)
+
+        //方法2：匿名函数的lamada表达式
+        /*handler.postDelayed({
+            //依赖anko库实现跳转函数
+            startActivity<LoginActivity>()
+            finish()
+        }, DELAY)*/
+    }
+
+    override fun onLoggedIn() {
+        //跳转主界面
+        startActivity<MainActivity>()
+        finish()
     }
 }
