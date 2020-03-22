1f5d5635f2c03799ddda445f135e4affdfdb95c7
gordon <gordon_sun07@163.com>
2020-03-22 17:34:03 +0800

    创建SplashActivity及配置全屏样式

---
+++ app/src/main/java/com/gordon/kotlin_im/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.gordon.kotlin_im
+
+/**
+ *    author : gordon
+ *    e-mail : gordon_sun07@163.com
+ *    date   : 2020/3/22 17:30
+ *    version: 1.0
+ *    desc   :
+ *
+ */
+class SplashActivity : BaseActivity() {
+    override fun getLayoutResId(): Int {
+        return R.layout.activity_splash
+    }
+}
