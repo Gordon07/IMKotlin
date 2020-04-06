d09d8c8a673afdb48322cb9639f8cee518b23d49
gordon <gordon_sun07@163.com>
2020-04-06 14:31:41 +0800

    1.注册界面View层实现

--- app/src/main/java/com/gordon/kotlin_im/RegisterActivity.kt
+++ app/src/main/java/com/gordon/kotlin_im/RegisterActivity.kt
@@ -1,5 +1,9 @@
 package com.gordon.kotlin_im
 
+import com.gordon.kotlin_im.contract.RegisterContract
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.toast
+
 /**
  *    author : gordon
  *    e-mail : gordon_sun07@163.com
@@ -8,8 +12,34 @@ package com.gordon.kotlin_im
  *    desc   :
  *
  */
-class RegisterActivity : BaseActivity() {
+class RegisterActivity : BaseActivity(), RegisterContract.View {
     override fun getLayoutResId(): Int {
         return R.layout.activity_register
+    }
+
+    override fun onUserNameError() {
+        userName.error = getString(R.string.user_name_error)
+    }
+
+    override fun onPasswordError() {
+        password.error = getString(R.string.password_error)
+    }
+
+    override fun onConfirmPasswordError() {
+        confirmPassword.error = getString(R.string.confirm_password_error)
+    }
+
+    override fun onStartRegister() {
+        showProgress(getString(R.string.registering))
+    }
+
+    override fun onRegisterSuccess() {
+        dismissProgress()
+        finish()
+    }
+
+    override fun onRegisterFailed() {
+        dismissProgress()
+        toast(getString(R.string.register_failed))
     }
 }
