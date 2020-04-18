12518404ec5d0ef76c44d71b7380bbb1acec34db
gordon <gordon_sun07@163.com>
2020-04-18 13:36:33 +0800

    1.FragmentFactory工厂类单例模式 2.切换页面

--- app/src/main/java/com/gordon/kotlin_im/ui/fragment/ContactFragment.kt
+++ app/src/main/java/com/gordon/kotlin_im/ui/fragment/ContactFragment.kt
@@ -1,7 +1,10 @@
 package com.gordon.kotlin_im.ui.fragment
 
+import android.view.View
 import com.gordon.kotlin_im.R
 import com.gordon.kotlin_im.ui.BaseFragment
+import kotlinx.android.synthetic.main.fragment_contacts.*
+import kotlinx.android.synthetic.main.header.*
 
 /**
  *    author : gordon
@@ -11,8 +14,17 @@ import com.gordon.kotlin_im.ui.BaseFragment
  *    desc   :
  *
  */
-class ContactFragment: BaseFragment() {
+class ContactFragment : BaseFragment() {
+
     override fun getLayoutResId(): Int {
         return R.layout.fragment_contacts
+    }
+
+    override fun init() {
+        super.init()
+        headerTitle.text = getString(R.string.contact)
+        add.visibility = View.VISIBLE
+        swipeRefreshLayout.setBackgroundColor(resources.getColor(R.color.qq_blue))
+
     }
 }
